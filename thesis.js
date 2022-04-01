9008f6d4591b62aa74c61c950351668ea2bfd7d5
shamailz <42952907+shamailz@users.noreply.github.com>
2022-04-01 04:22:05 -0400

    Update thesis.js

--- thesis.js
+++ thesis.js
@@ -303,21 +303,21 @@ class FirstPersonCameraDemo {
     plane.rotation.x = -Math.PI / 2;
     this.scene_.add(plane);
 
-    const box = new THREE.Mesh(
-      new THREE.BoxGeometry(2, 4, 50),
-      this.loadMaterial_('vintage-tile1_', 0.2));
-    box.position.set(3, 4, 0);
-    box.castShadow = true;
-    box.receiveShadow = true;
-    this.scene_.add(box);
+//     const box = new THREE.Mesh(
+//       new THREE.BoxGeometry(2, 4, 50),
+//       this.loadMaterial_('vintage-tile1_', 0.2));
+//     box.position.set(3, 4, 0);
+//     box.castShadow = true;
+//     box.receiveShadow = true;
+//     this.scene_.add(box);
     
-    const box2 = new THREE.Mesh(
-      new THREE.BoxGeometry(70, 4, 2),
-      this.loadMaterial_('vintage-tile1_', 0.2));
-    box.position.set(4, 2, 0);
-    box.castShadow = true;
-    box.receiveShadow = true;
-    this.scene_.add(box2);
+//     const box2 = new THREE.Mesh(
+//       new THREE.BoxGeometry(70, 4, 2),
+//       this.loadMaterial_('vintage-tile1_', 0.2));
+//     box.position.set(4, 2, 0);
+//     box.castShadow = true;
+//     box.receiveShadow = true;
+//     this.scene_.add(box2);
     
 //       const box3 = new THREE.Mesh(
 //       new THREE.BoxGeometry(2, 4, 40),
