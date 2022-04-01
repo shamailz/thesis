af334183fa501ab7ed887992e2a8da4c3b53118b
shamailz <42952907+shamailz@users.noreply.github.com>
2022-04-01 15:27:27 -0400

    Update thesis.js

--- thesis.js
+++ thesis.js
@@ -365,7 +365,7 @@ class FirstPersonCameraDemo {
     const wall4 = new THREE.Mesh(
       new THREE.BoxGeometry(5, 100, 300),
       concreteMaterial);
-    wall4.position.set(500, -40, 50);
+    wall4.position.set(0, -40, 0);
     wall4.castShadow = true;
     wall4.receiveShadow = true;
     this.scene_.add(wall4);
