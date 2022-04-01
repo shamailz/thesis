68a790e097e3ba1b4be4419f3e030b1547a189fd
shamailz <42952907+shamailz@users.noreply.github.com>
2022-04-01 13:47:14 -0400

    Update thesis.js

--- thesis.js
+++ thesis.js
@@ -339,7 +339,7 @@ class FirstPersonCameraDemo {
     const concreteMaterial = this.loadMaterial_('concrete3-', 4);
 
     const wall1 = new THREE.Mesh(
-      new THREE.BoxGeometry(150, 100, 5),
+      new THREE.BoxGeometry(200, 100, 5),
       concreteMaterial);
     wall1.position.set(0, -40, -90);
     wall1.castShadow = true;
@@ -347,7 +347,7 @@ class FirstPersonCameraDemo {
     this.scene_.add(wall1);
 
     const wall2 = new THREE.Mesh(
-      new THREE.BoxGeometry(150, 100, 5),
+      new THREE.BoxGeometry(200, 100, 5),
       concreteMaterial);
     wall2.position.set(50, -40, 10);
     wall2.castShadow = true;
@@ -355,7 +355,7 @@ class FirstPersonCameraDemo {
     this.scene_.add(wall2);
 
     const wall3 = new THREE.Mesh(
-      new THREE.BoxGeometry(5, 100, 150),
+      new THREE.BoxGeometry(5, 100, 200),
       concreteMaterial);
     wall3.position.set(0, -40, 50);
     wall3.castShadow = true;
@@ -363,7 +363,7 @@ class FirstPersonCameraDemo {
     this.scene_.add(wall3);
 
     const wall4 = new THREE.Mesh(
-      new THREE.BoxGeometry(5, 100, 150),
+      new THREE.BoxGeometry(5, 100, 200),
       concreteMaterial);
     wall4.position.set(0, -40, 50);
     wall4.castShadow = true;
