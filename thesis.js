874c7904150369aa5b48caee973c6d37190e55a5
shamailz <42952907+shamailz@users.noreply.github.com>
2022-04-01 03:52:27 -0400

    Update thesis.js

--- thesis.js
+++ thesis.js
@@ -317,7 +317,7 @@ class FirstPersonCameraDemo {
     box.position.set(4, 2, 0);
     box.castShadow = true;
     box.receiveShadow = true;
-    this.scene_.add(box);
+    this.scene_.add(box2);
     
       const box3 = new THREE.Mesh(
       new THREE.BoxGeometry(35, 4, 40),
@@ -325,7 +325,7 @@ class FirstPersonCameraDemo {
     box.position.set(7, 2, 0);
     box.castShadow = true;
     box.receiveShadow = true;
-    this.scene_.add(box);
+    this.scene_.add(box3);
 
       const box4 = new THREE.Mesh(
       new THREE.BoxGeometry(70, 4, 5),
@@ -333,13 +333,13 @@ class FirstPersonCameraDemo {
     box.position.set(9, 2, 0);
     box.castShadow = true;
     box.receiveShadow = true;
-    this.scene_.add(box);
+    this.scene_.add(box4);
 
 
     const concreteMaterial = this.loadMaterial_('concrete3-', 4);
 
     const wall1 = new THREE.Mesh(
-      new THREE.BoxGeometry(100, 100, 40),
+      new THREE.BoxGeometry(100, 100, 60),
       concreteMaterial);
     wall1.position.set(0, -40, -50);
     wall1.castShadow = true;
@@ -347,7 +347,7 @@ class FirstPersonCameraDemo {
     this.scene_.add(wall1);
 
     const wall2 = new THREE.Mesh(
-      new THREE.BoxGeometry(100, 100, 40),
+      new THREE.BoxGeometry(100, 100, 60),
       concreteMaterial);
     wall2.position.set(0, -40, 50);
     wall2.castShadow = true;
@@ -355,7 +355,7 @@ class FirstPersonCameraDemo {
     this.scene_.add(wall2);
 
     const wall3 = new THREE.Mesh(
-      new THREE.BoxGeometry(40, 100, 100),
+      new THREE.BoxGeometry(60, 100, 100),
       concreteMaterial);
     wall3.position.set(50, -40, 0);
     wall3.castShadow = true;
@@ -363,7 +363,7 @@ class FirstPersonCameraDemo {
     this.scene_.add(wall3);
 
     const wall4 = new THREE.Mesh(
-      new THREE.BoxGeometry(40, 100, 100),
+      new THREE.BoxGeometry(60, 100, 100),
       concreteMaterial);
     wall4.position.set(-50, -40, 0);
     wall4.castShadow = true;
