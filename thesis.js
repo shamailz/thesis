55b1577cc8b97c26f22a702fcb281bec43183532
shamailz <42952907+shamailz@users.noreply.github.com>
2022-04-01 18:51:33 -0400

    Update thesis.js

--- thesis.js
+++ thesis.js
@@ -314,7 +314,7 @@ class FirstPersonCameraDemo {
     const box2 = new THREE.Mesh(
       new THREE.BoxGeometry(100, 20, 5),
       this.loadMaterial_('vintage-tile1_', 0.2));
-    box.position.set(-25, 0, 0);
+    box.position.set(-25, 0, 10);
     box.castShadow = true;
     box.receiveShadow = true;
     this.scene_.add(box2);
