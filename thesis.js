b92c70d40dd416f9c68e3e71a310851201a96692
shamailz <42952907+shamailz@users.noreply.github.com>
2022-04-01 18:56:46 -0400

    Update thesis.js

--- thesis.js
+++ thesis.js
@@ -396,21 +396,21 @@ class FirstPersonCameraDemo {
   }
 
   initializeLights_() {
-    const distance = 9000.0;
-    const angle = Math.PI /2;
+    const distance = 7000.0;
+    const angle = Math.PI / 2;
     const penumbra = 0;
-    const decay = 0.8;
+    const decay = 1.0;
 
     let light = new THREE.SpotLight(
         0xFFFFFF, 500.0, distance, angle, penumbra, decay);
-    light.castShadow = false;
+    light.castShadow = true;
     light.shadow.bias = -0.00001;
     light.shadow.mapSize.width = 4096;
     light.shadow.mapSize.height = 4096;
     light.shadow.camera.near = 1;
     light.shadow.camera.far = 10;
 
-    light.position.set(25, 25, 0);
+    light.position.set(50, 50, 0);
     light.lookAt(0, 0, 0);
     this.scene_.add(light);
 
