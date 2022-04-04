ea4fe8d1e3b55d84dcf6df1770d26aa868ce4b4e
shamailz <42952907+shamailz@users.noreply.github.com>
2022-04-04 15:23:03 -0400

    Update thesis.js

--- thesis.js
+++ thesis.js
@@ -304,13 +304,13 @@ class FirstPersonCameraDemo {
     plane.rotation.x = -Math.PI / 2;
     this.scene_.add(plane);
     
-    const plane1 = new THREE.Mesh(
-      new THREE.PlaneGeometry (100 , 100 , 50, 50),
-      new THREE.MeshStandardMaterial({map: checkerboard}));
-      plane.castShadow = false;
-      plane.receiveShadow = true;
-      plane.rotation.x = -Math.PI / 2;
-      this.scene_.add(plane1);
+//     const plane1 = new THREE.Mesh(
+//       new THREE.PlaneGeometry (100 , 100 , 50, 50),
+//       new THREE.MeshStandardMaterial({map: checkerboard}));
+//       plane.castShadow = false;
+//       plane.receiveShadow = true;
+//       plane.rotation.x = -Math.PI / 2;
+//       this.scene_.add(plane1);
 
     const box = new THREE.Mesh(
       new THREE.BoxGeometry(5, 20, 150),
